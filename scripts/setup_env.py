7a8b8b046805cd2ce2bbfcba2c32bad623ff9881
Seongil Wi <su3604@kaist.ac.kr>
2018-09-29 23:19:31 +0900

    Add empty repo creation mechanism

--- scripts/setup_env.py
+++ scripts/setup_env.py
@@ -20,8 +20,39 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import json
+from utils import load_config
+from github import Github
 
-def setup_env(admin_config_file, token):
-    print 'setup'
 
+def create_repository(repo_owner, repo_name, github, description = None):
+    query = "/orgs/%s/repos" % (repo_owner)
 
+    repo = {"name": repo_name, "description": description}
+    r = github.post(query, json.dumps(repo), 201)
+
+    if r is None:
+        print '[*] Failed to create repository "%s"' % repo_name
+        print '[*] Response:', r
+    else:
+        print '[*] Successfully created repository "%s"' % repo_name
+
+def setup_env(admin_config_file, token=None):
+    admin_config = load_config(admin_config_file)
+    repo_owner = admin_config['repo_owner']
+    scoreboard_name = admin_config['scoreboard_name']
+
+    github = Github(admin_config["instructor"], token)
+
+    # Create scoreboard repo
+    print "[*] Creating scoreboard repository"
+    create_repository(repo_owner, scoreboard_name, github, "Scoreboard")
+    print
+
+    # Create problems repo
+    problems = admin_config['problems']
+    for problem in problems:
+        prob_repo_name = problems[problem]["repo_name"]
+        description = problems[problem]["description"]
+        print "[*] Creating %s repository" % prob_repo_name
+        create_repository(repo_owner, prob_repo_name, github, description)
